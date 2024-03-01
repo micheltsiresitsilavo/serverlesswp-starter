7e3abcb71e930eaaa4c366525c26bce81585e7a0
micheltsiresitsilavo <92079707+micheltsiresitsilavo@users.noreply.github.com>
2024-03-01 03:07:46 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const serverlesswp = require("serverlesswp-plus");
 
-const { validate } = require("../util/install.js");
+
 const { setup } = require("../util/directory.js");
 
 // This is where all requests to WordPress are routed through. See vercel.json or netlify.toml for the redirection rules.
@@ -10,13 +10,7 @@ exports.handler = async function (event, context, callback) {
 
   // Send the request (event object) to the serverlesswp library. It includes the PHP server that allows WordPress to handle the request.
   let response = await serverlesswp({ docRoot: "/tmp/wp", event: event });
-  // Check to see if the database environment variables are in place.
-  let checkInstall = validate(response);
-
-  if (checkInstall) {
-    return checkInstall;
-  } else {
     // Return the response for serving.
     return response;
-  }
+
 };
