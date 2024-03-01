fdface4b1105ed85cef0c04e11ef6455dd7d9e20
micheltsiresitsilavo <92079707+micheltsiresitsilavo@users.noreply.github.com>
2024-03-01 04:21:03 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -9,8 +9,7 @@ exports.handler = async function (event, context, callback) {
   setup();
 
   // Send the request (event object) to the serverlesswp library. It includes the PHP server that allows WordPress to handle the request.
-  let response = await serverlesswp({ docRoot: "/tmp/wp", event: event });
-    // Return the response for serving.
-    return response;
+   await serverlesswp({ docRoot: "/tmp/wp", event: event });
+ 
 
 };
